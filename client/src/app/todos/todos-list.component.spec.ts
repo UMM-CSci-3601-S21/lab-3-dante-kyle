b1da9aa694c225ef16024a34dff51daabe309c20
Dante <Mill8622@morris.umn.edu>
2021-02-18 13:40:45 -0700

    Creating HTML for filters, importing components, and creating methods
    HTML code is created for the filters. Components are then imported into various files. Methods are then created using methods in some of these components.
    Co-authored: Kyle Day <day00090@morris.umn.edu>

---
+++ client/src/app/todos/todos-list.component.spec.ts
@@ -0,0 +1,64 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatOptionModule } from '@angular/material/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Observable } from 'rxjs';
+import { MockUserService } from '../../testing/user.service.mock';
+import { Todos } from './todos';
+import { TodosListComponent } from './todos-list.component';
+import { TodosService } from './todos.service';
+
+
+const COMMON_IMPORTS: any[] = [
+  FormsModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatSelectModule,
+  MatOptionModule,
+  MatButtonModule,
+  MatInputModule,
+  MatExpansionModule,
+  MatTooltipModule,
+  MatListModule,
+  MatDividerModule,
+  MatRadioModule,
+  MatSnackBarModule,
+  BrowserAnimationsModule,
+  RouterTestingModule,
+];
+
+describe('TodosListComponent', () => {
+  let component: TodosListComponent;
+  let fixture: ComponentFixture<TodosListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TodosListComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TodosListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
+
---
+++ client/src/app/todos/todos-list.component.ts
@@ -0,0 +1,82 @@
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { throwError } from 'rxjs';
+import { Todos } from './todos';
+import { TodosService } from './todos.service';
+
+@Component({
+  selector: 'app-todos-list-component',
+  templateUrl: 'todos-list.component.html',
+  styleUrls: ['./todos-list.component.scss'],
+  providers: []
+})
+
+export class TodosListComponent implements OnInit {
+  // These are public so that tests can reference them (.spec.ts)
+  public serverFilteredTodos: Todos[];
+  public filteredTodos: Todos[];
+
+  public todosStatus: boolean;
+  public todosOwner: string;
+  public todosBody: string;
+  public todosCategory: string;
+  public viewType: 'card' | 'list' = 'card';
+
+  // Inject the UserService into this component.
+  // That's what happens in the following constructor.
+  //
+  // We can call upon the service for interacting
+  // with the server.
+  constructor(private todosService: TodosService, private snackBar: MatSnackBar) {
+
+  }
+
+  getTodosFromServer() {
+    this.todosService.getTodos({
+      status: this.todosStatus,
+      owner: this.todosOwner,
+      body: this.todosBody,
+      category: this.todosCategory
+    }).subscribe(returnedTodos => {
+      this.serverFilteredTodos = returnedTodos;
+      this.updateFilter();
+    }, err => {
+      // If there was an error getting the users, display
+      // a message.
+      this.snackBar.open(
+        'Problem contacting the server – try again',
+        'OK',
+        // The message will disappear after 3 seconds.
+        { duration: 3000 });
+      // I (Nic) feel like we should throw an error here, but
+      // I can't figure out how to test that at the moment,
+      // so I'm going to leave it out. If someone knows
+      // how to make this work that would be great.
+      //
+      // Now throw an error, which will show up in the browser
+      // JavaScript console and allow us to examine the stack
+      // trace.
+      //
+      // throw new Error('Failed to connect to the server: ' + err);
+    });
+  }
+
+  public updateFilter() {
+    this.filteredTodos = this.todosService.filterTodos(
+      this.serverFilteredTodos, { status: this.todosStatus,
+        owner: this.todosOwner,
+        body: this.todosBody,
+        category: this.todosCategory});
+  }
+
+  /**
+   * Starts an asynchronous operation to update the users list
+   *
+   */
+  ngOnInit(): void {
+    this.getTodosFromServer();
+  }
+}
+
+
+

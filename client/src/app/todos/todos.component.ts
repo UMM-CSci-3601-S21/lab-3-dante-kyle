575a5fdbf91b34506b627394d1f1f9e154c5d9ac
Dante <Mill8622@morris.umn.edu>
2021-02-18 13:43:55 -0700

    Creating HTML code to show each row, deleting code, importing components
    HTML code is created to show each row. Code that isn't necessary was deleted. Components are imported a file. These components are used to create code to showcase a table.
    Co-authored By: Kyle Day <day00090@morris.umn.edu>

--- client/src/app/todos/todos.component.ts
+++ client/src/app/todos/todos.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Todos } from './todos';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { TodosService } from './todos.service';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
+  providers: [TodosService],
   styleUrls: ['./todos.component.scss']
 })
 export class TodosComponent implements OnInit {
+    @Input() todos: Todos;
+    @Input() simple?: boolean = false;
+    dataSource;
 
-  constructor() { }
+    listData: MatTableModule;
+    displayedColumns: string[] = ['owner', 'category', 'status', 'body'];
+    todo: Todos[] = [];
 
-  ngOnInit(): void {
+
+
+    constructor(private http: TodosService) { };
+
+
+    ngOnInit(): void {
+      this.findTodos();
+    }
+
+    async findTodos() {
+
+
+      const arr = await this.http.getTodos().toPromise();
+      let i = 0;
+        while(arr[i]) {
+            this.todo.push(arr[i]);
+            i++;
+        }
+      this.dataSource = new MatTableDataSource<any>(this.todo);
   }
 
 }
+

998fdba24a9875f6a4885fcd7602c2024be51ae0
Kyle Day <day00090@morris.umn.edu>
2021-02-22 19:07:09 -0600

    Begin Testing for Todo Components and Remove todos.service.specs.ts
    This update adds spec-testing for the todo-list and todo components, along with testing for the todo service. The tests are not fully working, but they are expected to become functional in subsequent commits.  To finish off the refactoring, todos.service has been removed. 
    Co-authored-by: Dante Miller <mill8622@morris.umn.edu>

--- client/src/app/todos/todos.service.spec.ts
+++
@@ -1,5 +0,0 @@
-import { HttpClient } from '@angular/common/http';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { TestBed } from '@angular/core/testing';
-import { Todos } from './todos';
-import { TodosService } from './todos.service';

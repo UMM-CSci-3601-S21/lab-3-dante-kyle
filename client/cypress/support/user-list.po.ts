d532452c3a71ed21ba4eedda129c5a1600b32075
Kyle Day <day00090@morris.umn.edu>
2021-02-23 13:54:21 -0600

    Fixed Issue Caused by Linting in user-list.po.ts
    This fixes an issue with the linting flagging incorrectly formatted comments as errors.
    Co-authored-by: Dante Miller <mill6322@morris.umn.edu>

--- client/cypress/support/user-list.po.ts
+++ client/cypress/support/user-list.po.ts
@@ -19,27 +19,21 @@ export class UserListPage {
     return cy.get('.user-nav-list .user-list-item');
   }
 
-  /**
-   * Clicks the "view profile" button for the given user card.
-   * Requires being in the "card" view.
-   * @param card The user card
-   */
+  /*Clicks the "view profile" button for the given user card.
+   * Requires being in the "card" view.*/
+  /*@param card The user card*/
   clickViewProfile(card: Cypress.Chainable<JQuery<HTMLElement>>) {
     return card.find<HTMLButtonElement>('[data-test=viewProfileButton]').click();
   }
 
-  /**
-   * Change the view of users.
-   * @param viewType Which view type to change to: "card" or "list".
-   */
+  /*Change the view of users.*/
+   /*@param viewType Which view type to change to: "card" or "list".*/
   changeView(viewType: 'card' | 'list') {
     return cy.get(`[data-test=viewTypeRadio] .mat-radio-button[value="${viewType}"]`).click();
   }
 
-  /**
-   * Selects a role to filter in the "Role" selector.
-   * @param value The role *value* to select, this is what's found in the mat-option "value" attribute.
-   */
+  /* Selects a role to filter in the "Role" selector.*/
+  /* @param value The role *value* to select, this is what's found in the mat-option "value" attribute.*/
   selectRole(value: string) {
     return cy.get('[data-test=userRoleSelect]').click().get(`mat-option[value="${value}"]`).click();
   }
